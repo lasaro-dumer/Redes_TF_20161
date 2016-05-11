79a1eef412e56410cbdcf8cb1aef7b392291d280
João <jpchagas@gmail.com>
2016-05-11 17:05:39 -0300

    :):)

--- Reader.java
+++ Reader.java
@@ -1,21 +1,22 @@
+package TF;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Reader{
 	public String read(String file){
+		StringBuilder sb = new StringBuilder();
 		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
-    		StringBuilder sb = new StringBuilder();
     		String line = br.readLine();
-
     		while (line != null) {
         		sb.append(line);
         		sb.append(System.lineSeparator());
         		line = br.readLine();
     		}
-    		String everything = sb.toString();
 		}catch (IOException e) {
 			e.printStackTrace();
-		} 
+		}
+		return sb.toString();
 	}
 }
--- Simulador.java
+++ Simulador.java
@@ -1,5 +1,14 @@
-public class Simulador{
-	public static void main(String file,String origem,String destino,String mensagem){
+package TF;
 
+public class Simulador{
+	String file , origem, destino,mensagem;
+	public static void main(String file,String origem,String destino,String mensagem){
+		this.file = file;
+		this.origem = origem;
+		this.destino = destino;
+		this.mensagem = mensagem;
+
+		Reader r = new Reader();
+		r.read(file);
 	}
 }
