77a8f7503ad60b96890f9509a7167d81ba1cea71
Lásaro Dumer <ladumer@hotmail.com>
2016-05-11 19:14:03 -0300

    Fixed Simulador.java Fixed parameters Fixed Reader output

--- Simulador.java
+++ Simulador.java
@@ -1,12 +1,12 @@
 public class Simulador{
 	String file , origem, destino,mensagem;
-	public static void main(String file,String origem,String destino,String mensagem){
-		String filename = file;
-		String n1 = origem;
-		String n2 = destino;
-		String m = mensagem;
+	public static void main(String[] args){
+		String filename = args[0];
+		//String n1 = origem;
+		//String n2 = destino;
+		///String m = mensagem;
 
 		Reader r = new Reader();
-		r.read(file);
+		System.out.println(r.read(filename));
 	}
 }
