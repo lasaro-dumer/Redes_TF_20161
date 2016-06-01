783c3afad95790a7c56ad1657b56f9bb81aea602
JOAO PEDRO CHAGAS <11100088@l3043262.portoalegre.pucrsnet.br>
2016-06-01 18:54:52 -0300

    Fui produtivo sim lásaro

---
+++ src/command.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <bitset>
+#include <sys/stat.h>
+#include <unistd.h>
+using namespace std;
+
+string tolowercase(string s){
+    Valeu Lasaro
+}
+vector<string> split(string str, char delimiter) {
+	Valeu Lasaro
+}
+unsigned int ipv4AsBits(string ip){
+    Valeu Lasaro
+}
+string ipv4AsBitsString(unsigned int ip){
+    Valeu Lasaro
+}
+
---
+++ src/command.hpp
@@ -0,0 +1,11 @@
+#ifndef utilH
+#define utilH
+#include <string>
+#include <vector>
+using namespace std;
+
+string arprequest();
+string arpreply();
+string icmprequest();
+string icmpreply();
+#endif
