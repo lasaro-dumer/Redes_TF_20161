1a5681b0a071c268ba5c7f23fde1400f74fa4f49
João Pedro <jpchagas@gmail.com>
2016-06-18 16:36:01 -0300

    Slice

--- src/ICMPPackage.hpp
+++ src/ICMPPackage.hpp
@@ -13,12 +13,16 @@ public:
     ipv4* dstHop_IP;
     string srcHop_MAC,srcHop_Name,dstHop_MAC,dstHop_Name;
     string message,type;
-    int TTL;
+    bool moreFragments;
+    int TTL, offset;
     //ICMPPackage(src_IP,srcHop_IP,srcHop_MAC,srcHop_Name,dst_IP,dstHop_IP,dstHop_MAC,dstHop_Name,message,type,ttl)
     ICMPPackage(ipv4* src,ipv4* srcHpI,string srcM,string srcN,ipv4* dst,ipv4* dstHpI,string dstM,string dstN,string m,string t,int _ttl)
                 :src_IP(src),srcHop_IP(srcHpI),srcHop_MAC(srcM),srcHop_Name(srcN)
                 ,dst_IP(dst),dstHop_IP(dstHpI),dstHop_MAC(dstM),dstHop_Name(dstN)
-                ,message(m),type(t),TTL(_ttl){}
+                ,message(m),type(t),TTL(_ttl){
+                offset = 0;
+                moreFragments = false;
+                }
 
     void updateDataLinkInfo(string srcHopName,string srcHopMAC,string dstHopName,string dstHopMAC);
     string toString();
@@ -34,17 +38,25 @@ public:
     }
 
     static ICMPPackage remountMessage(vector<ICMPPackage> packages){
-        //TODO: Implement proper remountMessage....
-        return packages[0];
+            //TODO
     }
 
     static vector<ICMPPackage> sliceMessage(ICMPPackage package,int maxMTU){
         vector<ICMPPackage> packages;
-        if(maxMTU > 0){
-            //TODO: Implement proper sliceMessage....
-        }else{
-            packages.push_back(package);
+
+        int message_s = package.message.size();
+        int npackages = ceil(message_s/maxMTU);
+
+        for(int =0;i<npackages;i++){
+            int noffset = i*maxMTU;
+            string nmessage = message.substr(noffset,maxMTU);
+            ICMPPackage ping(src_IP,srcHop_IP,srcHop_MAC,srcHop_Name,dst_IP,dstHop_IP,dstHop_MAC,dstHop_Name,nmessage,type,ttl);
+            noffset += package.offset;
+            ping.offset = noffset;                      
+            ping.moreFragments = ((i+1)!=npackages) || package.moreFragments;
+            packages.push_back(ping);
         }
+
         return packages;
     }
 
